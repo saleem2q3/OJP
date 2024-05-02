6d4d31562bfd39ca043ddd7fa33fc64f634d9ba0
saleem2q3 <ssaleem2409@gmail.com>
2024-05-02 11:40:18 +0530

    final commit

--- onlinejobportal/urls.py
+++ onlinejobportal/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('employermodule/',  include('employermodule.urls'))
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
