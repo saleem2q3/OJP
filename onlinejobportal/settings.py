24a9c245b2d1a2de047dd3b2e6f20cfde1da130c
saleem2q3 <ssaleem2409@gmail.com>
2024-05-14 08:03:39 +0530

    final commit

--- onlinejobportal/settings.py
+++ onlinejobportal/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static', ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "homepage"
 LOGOUT_REDIRECT_URL = "login"
